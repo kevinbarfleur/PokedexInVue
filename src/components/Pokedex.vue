f5df5f00a1635a3bb706f945a67560728d04fdec
Kévin Barfleur <kevinbarfleur@gmail.com>
2020-01-05 00:07:52 +0100

    cards update

--- src/components/Pokedex.vue
+++ src/components/Pokedex.vue
@@ -1,10 +1,61 @@
 <template>
   <div class="pokedex-container">
-    <h1 class="main-title">Pokedex</h1>
-    <p class="main-subtitle">Basic pokedex in Vue.js</p>
+    <header class="header">
+      <h1 class="main-title">Pokedex</h1>
+      <p class="main-subtitle">Basic pokedex in Vue.js</p>
+    </header>
+    <div class="cards-container">
+      <div class="card" v-for="pokemon in pokemons" :key="pokemon.id">
+        <PokemonCard :name="pokemon.name" :sprite="pokemon.sprites.front_default"></PokemonCard>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+import PokemonCard from './PokemonCard'
+
+export default {
+  components: {
+    PokemonCard
+  },
+  props: {
+    name: String,
+    sprite: String
+  },
+  data() {
+    return {
+      pokemons: []
+    }
+  },
+  mounted() {
+    this.getDatas()
+  },
+  methods: {
+    getDatas() {
+      for (let i = 1; i <= 151; i++) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`
+        fetch(url)
+          .then(res => res.json())
+          .then(pokemon => {
+            this.pokemons.push(pokemon)
+          })
+      }
+    }
+  }
+}
 </script>
+
+<style lang="scss">
+.pokedex-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.cards-container {
+  display: flex;
+  justify-content: space-evenly;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+</style>
---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="pokemon-card">
+    <h3 class="pokemon-name">{{ this.pokemonName }}</h3>
+    <img :src="this.pokemonSprite" :alt="'sprite ' + this.pokemonName" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    sprite: String
+  },
+  data() {
+    return {
+      pokemonName: '',
+      pokemonSprite: ''
+    }
+  },
+  mounted() {
+    this.getProps()
+  },
+  methods: {
+    getProps() {
+      this.pokemonName = this.name
+      this.pokemonSprite = this.sprite
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.pokemon-card {
+  max-width: 400px;
+  min-width: 200px;
+
+  padding: 5%;
+  margin: 10% auto;
+
+  border: 1px solid #fafafa;
+  border-radius: 10px;
+  box-shadow: 2px 2px 25px lightgrey;
+
+  .pokemon-name {
+  }
+}
+</style>
