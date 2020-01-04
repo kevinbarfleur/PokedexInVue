7e949e025bf82b08e70d992ae128a82ee5f57296
Kévin Barfleur <kevinbarfleur@gmail.com>
2020-01-04 22:41:27 +0100

    first commit

---
+++ src/components/Pokedex.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="pokedex-container">
+    <h1 class="main-title">Pokedex</h1>
+    <p class="main-subtitle">Basic pokedex in Vue.js</p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
