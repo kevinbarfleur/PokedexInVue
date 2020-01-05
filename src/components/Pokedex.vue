8690195e05b28667acde915016aedbb0c9493a5d
Kévin Barfleur <kevinbarfleur@gmail.com>
2020-01-05 21:42:17 +0100

    catch API

--- src/components/Pokedex.vue
+++ src/components/Pokedex.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="pokedex-container">
     <header class="header">
-      <h1 class="main-title">Pokedex</h1>
+      <div class="main-title">
+        <h1>Pokedex</h1>
+        <img class="icon" src="../assets/pokeball.png" alt />
+      </div>
       <p class="main-subtitle">Basic pokedex in Vue.js</p>
     </header>
-    <div class="cards-container">
-      <div class="card" v-for="pokemon in pokemons" :key="pokemon.id">
-        <PokemonCard :name="pokemon.name" :sprite="pokemon.sprites.front_default"></PokemonCard>
+    <transition name="fade" mode="out-in">
+      <div class="loading" v-if="pokemonsLength !== 151" key="loading">{{ pokemonsLength }}</div>
+      <div class="cards-container" v-else key="cards">
+        <div class="card" v-for="pokemon in pokemons" :key="pokemon.id">
+          <PokemonCard :name="pokemon.name" :sprite="pokemon.sprites.front_default" />
+        </div>
       </div>
-    </div>
+    </transition>
   </div>
 </template>
 
@@ -42,20 +48,70 @@ export default {
           })
       }
     }
+  },
+  computed: {
+    pokemonsLength() {
+      // 151 * 100 % 151 = 100%
+      return this.pokemons.length
+    }
   }
 }
 </script>
 
 <style lang="scss">
+.main-title {
+  display: flex;
+  flex-direction: row;
+
+  font-family: 'Press Start 2P', cursive;
+
+  .icon {
+    margin: auto;
+    margin-left: 10px;
+    width: 50px;
+    height: 50px;
+  }
+}
 .pokedex-container {
   display: flex;
   flex-direction: column;
   justify-content: center;
+  align-items: center;
+}
+.loading {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateX(-50%) translateY(-50%);
+  border: 1px solid grey;
+  width: 200px;
+  padding: 5px 0px;
+  border-radius: 10px;
 }
 .cards-container {
+  margin: 100px auto;
+  width: 80%;
   display: flex;
-  justify-content: space-evenly;
+  justify-content: center;
   flex-direction: row;
   flex-wrap: wrap;
+}
+.card {
+  min-width: 200px;
+  margin: auto 5%;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition-property: opacity;
+  transition-duration: 0.5s;
+}
+
+.fade-enter-active {
+  transition-delay: 0.25s;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
 }
 </style>
--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -2,6 +2,12 @@
   <div class="pokemon-card">
     <h3 class="pokemon-name">{{ this.pokemonName }}</h3>
     <img :src="this.pokemonSprite" :alt="'sprite ' + this.pokemonName" />
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis sit nam maxime. Accusamus, veritatis tempora harum voluptatem ut perspiciatis provident.</p>
+    <!-- <button class="card-button">Show More</button> -->
+    <div id="button">
+      <div id="slide"></div>
+      <a href="#">Show More &#8594;</a>
+    </div>
   </div>
 </template>
 
@@ -22,8 +28,11 @@ export default {
   },
   methods: {
     getProps() {
-      this.pokemonName = this.name
+      this.pokemonName = this.formatName(this.name)
       this.pokemonSprite = this.sprite
+    },
+    formatName(name) {
+      return name.charAt(0).toUpperCase() + name.substr(1)
     }
   }
 }
@@ -31,17 +40,71 @@ export default {
 
 <style lang="scss">
 .pokemon-card {
-  max-width: 400px;
+  //   cursor: pointer;
+  max-width: 300px;
   min-width: 200px;
 
   padding: 5%;
-  margin: 10% auto;
+  margin: 20% auto;
 
   border: 1px solid #fafafa;
   border-radius: 10px;
   box-shadow: 2px 2px 25px lightgrey;
+  transition: 0.3s;
 
-  .pokemon-name {
+  &:hover {
+    transform: translateY(2px) translateX(2px);
+    box-shadow: 1px 1px 10px lightgrey;
+    transition: 0.3s;
+  }
+
+  .card-button {
+  }
+
+  /* Second Button */
+
+  #button {
+    position: relative;
+    overflow: hidden;
+    cursor: pointer;
+
+    display: inline-flex;
+    // height: 40px;
+    // width: 150px;
+    border: 1px solid #4fc3f7;
+    margin: 10px auto;
+    text-decoration: none;
+    align-items: center;
+    justify-content: center;
+    overflow: hidden;
+
+    border-radius: 2px;
+  }
+
+  #button a {
+    text-decoration: none;
+    color: #4fc3f7;
+    position: relative;
+    transition: all 0.35s ease-Out;
+    padding: 1px 10px;
+  }
+
+  #slide {
+    width: 100%;
+    height: 100%;
+    left: -200px;
+    background: #4fc3f7;
+    position: absolute;
+    transition: all 0.35s ease-Out;
+    bottom: 0;
+  }
+
+  #button:hover #slide {
+    left: 0;
+  }
+
+  #button:hover a {
+    color: white;
   }
 }
 </style>
