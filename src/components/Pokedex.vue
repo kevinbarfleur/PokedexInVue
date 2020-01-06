fa490e207b5685489acdcae0749a8f150ea6e8ae
Kévin Barfleur <kevinbarfleur@gmail.com>
2020-01-07 00:26:09 +0100

    add types, colors, filters

--- src/components/Pokedex.vue
+++ src/components/Pokedex.vue
@@ -7,11 +7,24 @@
       </div>
       <p class="main-subtitle">Basic pokedex in Vue.js</p>
     </header>
+    <div class="filters">
+      <div class="checkbox-container" v-for="type in types" :key="type">
+        <input type="checkbox" :name="type" :id="type" v-model="type.filters" />
+        <label :for="type">{{ type }}</label>
+      </div>
+    </div>
     <transition name="fade" mode="out-in">
-      <div class="loading" v-if="pokemonsLength !== 151" key="loading">{{ pokemonsLength }}</div>
+      <div class="loading" v-if="pokemonsLength !== 151" key="loading">
+        <div class="loading-bar"></div>
+        {{ percentage + "%" }}
+      </div>
       <div class="cards-container" v-else key="cards">
         <div class="card" v-for="pokemon in pokemons" :key="pokemon.id">
-          <PokemonCard :name="pokemon.name" :sprite="pokemon.sprites.front_default" />
+          <PokemonCard
+            :name="pokemon.name"
+            :sprite="pokemon.sprites.front_default"
+            :type="pokemon.types[0].type.name"
+          />
         </div>
       </div>
     </transition>
@@ -31,11 +44,14 @@ export default {
   },
   data() {
     return {
-      pokemons: []
+      pokemons: [],
+      types: [],
+      filters: []
     }
   },
   mounted() {
     this.getDatas()
+    this.$set(this, this.filters, this.types)
   },
   methods: {
     getDatas() {
@@ -44,15 +60,34 @@ export default {
         fetch(url)
           .then(res => res.json())
           .then(pokemon => {
-            this.pokemons.push(pokemon)
+            setTimeout(() => {
+              this.pokemons.push(pokemon)
+
+              const formatedType = this.formatUpper(pokemon.types[0].type.name)
+
+              if (!this.types.includes(formatedType)) {
+                this.types.push(formatedType)
+              }
+            }, 1000)
           })
       }
+    },
+    formatUpper(name) {
+      return name.charAt(0).toUpperCase() + name.substr(1)
+    },
+    updateFilters(filter) {
+      console.log(filter)
     }
   },
   computed: {
     pokemonsLength() {
-      // 151 * 100 % 151 = 100%
       return this.pokemons.length
+    },
+    typesLenght() {
+      return this.types.length
+    },
+    percentage() {
+      return Math.round((100 * this.pokemonsLength) / 150)
     }
   }
 }
@@ -72,6 +107,95 @@ export default {
     height: 50px;
   }
 }
+.filters {
+  position: fixed;
+  top: 50%;
+  left: -10px;
+  display: flex;
+  flex-direction: column;
+  transform: translateY(-50%);
+  /*  CHECKBOX ANIMATION  */
+
+  .checkbox-container {
+    background-color: #fff;
+    display: block;
+    position: relative;
+
+    label {
+      padding: 12px 30px;
+      width: 100%;
+      display: block;
+      text-align: left;
+      color: #3c454c;
+      cursor: pointer;
+      position: relative;
+      z-index: 2;
+      transition: color 200ms ease-in;
+      overflow: hidden;
+
+      &:before {
+        width: 10px;
+        height: 10px;
+        border-radius: 50%;
+        content: '';
+        background-color: #5562eb;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%) scale3d(1, 1, 1);
+        transition: all 300ms cubic-bezier(0.4, 0, 0.2, 1);
+        opacity: 0;
+        z-index: -1;
+      }
+
+      &:after {
+        width: 32px;
+        height: 32px;
+        content: '';
+        border: 2px solid #d1d7dc;
+        background-color: #fff;
+        background-image: url("data:image/svg+xml,%3Csvg width='32' height='32' viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M5.414 11L4 12.414l5.414 5.414L20.828 6.414 19.414 5l-10 10z' fill='%23fff' fill-rule='nonzero'/%3E%3C/svg%3E ");
+        background-repeat: no-repeat;
+        background-position: 2px 3px;
+        border-radius: 50%;
+        z-index: 2;
+        position: absolute;
+        right: 30px;
+        top: 50%;
+        transform: translateY(-50%);
+        cursor: pointer;
+        transition: all 200ms ease-in;
+      }
+    }
+
+    input:checked ~ label {
+      color: #fff;
+
+      &:before {
+        transform: translate(-50%, -50%) scale3d(56, 56, 1);
+        opacity: 1;
+      }
+
+      &:after {
+        background-color: #54e0c7;
+        border-color: #54e0c7;
+      }
+    }
+
+    input {
+      width: 32px;
+      height: 32px;
+      order: 1;
+      z-index: 2;
+      position: absolute;
+      right: 30px;
+      top: 50%;
+      transform: translateY(-50%);
+      cursor: pointer;
+      visibility: hidden;
+    }
+  }
+}
 .pokedex-container {
   display: flex;
   flex-direction: column;
@@ -83,10 +207,24 @@ export default {
   top: 50%;
   left: 50%;
   transform: translateX(-50%) translateY(-50%);
-  border: 1px solid grey;
   width: 200px;
   padding: 5px 0px;
   border-radius: 10px;
+
+  color: #212121;
+  font-size: 1.1em;
+  font-family: 'Press Start 2P', cursive;
+
+  .loading-bar {
+    z-index: -1;
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    width: 100%;
+    height: 100%;
+    background-color: #f4511e;
+    border-radius: 9px;
+  }
 }
 .cards-container {
   margin: 100px auto;
--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="pokemon-card">
     <h3 class="pokemon-name">{{ this.pokemonName }}</h3>
+    <p>
+      Type :
+      <span :style="{ color: this.pokemonColorType }">{{ this.formatUpper(this.pokemonType) }}</span>
+    </p>
     <img :src="this.pokemonSprite" :alt="'sprite ' + this.pokemonName" />
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis sit nam maxime. Accusamus, veritatis tempora harum voluptatem ut perspiciatis provident.</p>
-    <!-- <button class="card-button">Show More</button> -->
-    <div id="button">
-      <div id="slide"></div>
-      <a href="#">Show More &#8594;</a>
+    <div id="button" :style="{ borderColor: this.pokemonColorType }">
+      <div id="slide" :style="{ backgroundColor: this.pokemonColorType }"></div>
+      <a href="#" :style="{ color: this.pokemonColorType }">Show More &#8594;</a>
     </div>
   </div>
 </template>
@@ -15,24 +18,83 @@
 export default {
   props: {
     name: String,
-    sprite: String
+    type: String,
+    sprite: String,
+    changeView: Function
   },
   data() {
     return {
       pokemonName: '',
+      pokemonType: '',
+      pokemonColorType: '',
       pokemonSprite: ''
     }
   },
   mounted() {
     this.getProps()
+    this.typeToColor()
   },
   methods: {
     getProps() {
-      this.pokemonName = this.formatName(this.name)
+      this.pokemonName = this.formatUpper(this.name)
+      this.pokemonType = this.type
       this.pokemonSprite = this.sprite
     },
-    formatName(name) {
+    formatUpper(name) {
       return name.charAt(0).toUpperCase() + name.substr(1)
+    },
+    typeToColor() {
+      switch (this.pokemonType) {
+        case 'poison':
+          this.pokemonColorType = '#9C27B0'
+          break
+        case 'fire':
+          this.pokemonColorType = '#FF3D00'
+          break
+        case 'flying':
+          this.pokemonColorType = '#BBDEFB'
+          break
+        case 'water':
+          this.pokemonColorType = '#2196F3'
+          break
+        case 'bug':
+          this.pokemonColorType = '#9E9D24'
+          break
+        case 'steel':
+          this.pokemonColorType = '#546E7A'
+          break
+        case 'normal':
+          this.pokemonColorType = '#9E9E9E'
+          break
+        case 'electric':
+          this.pokemonColorType = '#FDD835'
+          break
+        case 'ground':
+          this.pokemonColorType = '#8D6E63'
+          break
+        case 'ice':
+          this.pokemonColorType = '#80DEEA'
+          break
+        case 'fairy':
+          this.pokemonColorType = '#F48FB1'
+          break
+        case 'grass':
+          this.pokemonColorType = '#43A047'
+          break
+        case 'fighting':
+          this.pokemonColorType = '#212121'
+          break
+        case 'psychic':
+          this.pokemonColorType = '#311B92'
+          break
+        case 'rock':
+          this.pokemonColorType = '#4E342E'
+          break
+
+        default:
+          this.pokemonColorType = '#f4511e'
+          break
+      }
     }
   }
 }
@@ -40,13 +102,13 @@ export default {
 
 <style lang="scss">
 .pokemon-card {
-  //   cursor: pointer;
   max-width: 300px;
   min-width: 200px;
 
   padding: 5%;
   margin: 20% auto;
 
+  background-color: white;
   border: 1px solid #fafafa;
   border-radius: 10px;
   box-shadow: 2px 2px 25px lightgrey;
@@ -58,20 +120,13 @@ export default {
     transition: 0.3s;
   }
 
-  .card-button {
-  }
-
-  /* Second Button */
-
   #button {
     position: relative;
     overflow: hidden;
     cursor: pointer;
 
     display: inline-flex;
-    // height: 40px;
-    // width: 150px;
-    border: 1px solid #4fc3f7;
+    border: 1px solid #f4511e;
     margin: 10px auto;
     text-decoration: none;
     align-items: center;
@@ -83,7 +138,7 @@ export default {
 
   #button a {
     text-decoration: none;
-    color: #4fc3f7;
+    color: #f4511e;
     position: relative;
     transition: all 0.35s ease-Out;
     padding: 1px 10px;
@@ -93,7 +148,7 @@ export default {
     width: 100%;
     height: 100%;
     left: -200px;
-    background: #4fc3f7;
+    background: #f4511e;
     position: absolute;
     transition: all 0.35s ease-Out;
     bottom: 0;
@@ -104,7 +159,7 @@ export default {
   }
 
   #button:hover a {
-    color: white;
+    color: white !important;
   }
 }
 </style>
